d6dff9eb536124de085f35c7a2215ee54d73177d
Fabrice Bacchella <fbacchella@spamcop.net>
2016-06-25 00:23:42 +0200

    Size test failing after new index formatting.

--- src/test/java/fr/jrds/SmiExtensions/types/SizeTest.java
+++ src/test/java/fr/jrds/SmiExtensions/types/SizeTest.java
@@ -10,25 +10,25 @@ public class SizeTest {
     @Test
     public void one() {
         Size s = new Size("4");
-        Assert.assertEquals(null, s.extract(new int[]{1, 2, 3}));
-        Assert.assertEquals(4, s.extract(new int[]{1, 2, 3, 4}).length);
-        Assert.assertEquals(4, s.extract(new int[]{1, 2, 3, 4, 5}).length);
+        Assert.assertEquals(null, s.extract(new int[]{1, 2, 3}).content);
+        Assert.assertEquals(4, s.extract(new int[]{1, 2, 3, 4}).content.length);
+        Assert.assertEquals(4, s.extract(new int[]{1, 2, 3, 4, 5}).content.length);
     }
 
     @Test
     public void two() {
         Size s = new Size("4 | 8");
-        Assert.assertEquals(4, s.extract(new int[]{1, 2, 3, 4}).length);
-        Assert.assertEquals(4, s.extract(new int[]{1, 2, 3, 4, 5}).length);
-        Assert.assertEquals(8, s.extract(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}).length);
+        Assert.assertEquals(4, s.extract(new int[]{4, 1, 2, 3, 4}).content.length);
+        Assert.assertEquals(4, s.extract(new int[]{4, 1, 2, 3, 4, 5}).content.length);
+        Assert.assertEquals(8, s.extract(new int[]{8, 1, 2, 3, 4, 5, 6, 7, 8, 9}).content.length);
     }
 
     @Test
     public void three() {
         Size s = new Size("4..8");
-        Assert.assertEquals(4, s.extract(new int[]{1, 2, 3, 4}).length);
-        Assert.assertEquals(5, s.extract(new int[]{1, 2, 3, 4, 5}).length);
-        Assert.assertEquals(8, s.extract(new int[]{1, 2, 3, 4, 5, 6, 7, 8}).length);
-        Assert.assertEquals(8, s.extract(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}).length);
+        Assert.assertEquals(4, s.extract(new int[]{4, 1, 2, 3, 4}).content.length);
+        Assert.assertEquals(5, s.extract(new int[]{5, 1, 2, 3, 4, 5}).content.length);
+        Assert.assertEquals(8, s.extract(new int[]{8, 1, 2, 3, 4, 5, 6, 7, 8}).content.length);
+        Assert.assertEquals(8, s.extract(new int[]{8, 1, 2, 3, 4, 5, 6, 7, 8, 9}).content.length);
     }
 }
