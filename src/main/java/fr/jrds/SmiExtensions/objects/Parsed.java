d7e85878d37540aa694e78e2fe49df38ff53f2f9
Fabrice Bacchella <fbacchella@spamcop.net>
2016-06-27 18:45:02 +0200

    Improved debuging with Parsed object

--- src/main/java/fr/jrds/SmiExtensions/objects/Parsed.java
+++ src/main/java/fr/jrds/SmiExtensions/objects/Parsed.java
@@ -1,6 +1,13 @@
 package fr.jrds.SmiExtensions.objects;
 
+import fr.jrds.SmiExtensions.Utils;
+
 public class Parsed {
     public int[] content = null;
     public int[] next = null;
+    @Override
+    public String toString() {
+        return (content != null ? Utils.dottedNotation(content) : "") + "/" + (next != null ? Utils.dottedNotation(next) : "");
+    }
+
 }
