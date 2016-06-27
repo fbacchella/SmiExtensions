ab1222ad05b7b8b1616645c28c530ed586a5bb8b
Fabrice Bacchella <fbacchella@spamcop.net>
2016-06-27 16:44:20 +0200

    Unsigned long test was failling.

--- src/test/java/fr/jrds/SmiExtensions/UnsignedTest.java
+++ src/test/java/fr/jrds/SmiExtensions/UnsignedTest.java
@@ -12,7 +12,7 @@ public class UnsignedTest {
     public void one() {
         Counter64 v = new Counter64(-1);
         UnsignedLong n = Utils.getUnsigned(v.getValue());
-        Assert.assertEquals("18446744073709551615",n);
+        Assert.assertEquals("18446744073709551615",n.toString());
         Assert.assertEquals(1,n.compareTo(Long.MAX_VALUE));
         
     }
